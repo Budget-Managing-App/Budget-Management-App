0f8f310e2db05ba1d15b5c3cd7b668fd3200e996
KiranDev12 <kirandev.edu@gmail.com>
2023-05-22 14:30:10 +0530

    Integration of Login and register check

--- app/src/main/java/com/example/budgetmanagementapp/Login.kt
+++ app/src/main/java/com/example/budgetmanagementapp/Login.kt
@@ -1,11 +1,87 @@
 package com.example.budgetmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    lateinit var editTextEmail : TextInputEditText
+    lateinit var editTextPassword : TextInputEditText
+    lateinit var buttonLogin : Button
+    lateinit var auth: FirebaseAuth
+    lateinit var progressBar: ProgressBar
+    lateinit var textView: TextView
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        buttonLogin = findViewById(R.id.buttonlogin)
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.registerNow)
+        textView.setOnClickListener{
+            val intent = Intent(applicationContext, Register::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonLogin.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email: String = editTextEmail.text.toString()
+            val password: String = editTextPassword.text.toString()
+
+            if (TextUtils.isEmpty(email)){
+                Toast.makeText(this@Login, "Enter email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)){
+                Toast.makeText(this@Login, "Enter password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(
+                            this@Login,
+                            "Login Successful.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                        val intent = Intent(applicationContext, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            this@Login,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
     }
 }
